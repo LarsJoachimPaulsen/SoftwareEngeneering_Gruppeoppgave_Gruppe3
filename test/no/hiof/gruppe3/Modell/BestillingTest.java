75bd91896908efaf16ebec8b91f22f73373f02d7
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-10-24 14:33:46 +0200

    Lagt inn parameterized tests

--- test/no/hiof/gruppe3/Modell/BestillingTest.java
+++ test/no/hiof/gruppe3/Modell/BestillingTest.java
@@ -1,6 +1,8 @@
 package no.hiof.gruppe3.Modell;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,14 +10,28 @@ class BestillingTest {
 
     Bestilling bestilling = new Bestilling();
 
-    @Test
-    void en_bestilling_skal_redusere_antall_billetter_tiljengelig_test(){
-
-        assertEquals("Det finnes desverre ikke nok billetter", bestilling.bestillBillet(103));
-        assertEquals("Bestilling vellykket", bestilling.bestillBillet(100));
-        assertEquals("Det er desverre tomt for billetter", bestilling.bestillBillet(2));
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 5, 9 })
+    void en_bestilling_skal_gi_tilbakemelding_om_at_den_er_vellykket_test(int antallBilletter){
+        assertEquals("Bestilling vellykket", bestilling.bestillBillet(antallBilletter));
 
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {101, 103, 109, 134})
+    void en_bestilling_skal_si_ifra_om_det_ikke_er_nok_billetter_test(int antallBilleter){
+        assertEquals("Det finnes desverre ikke nok billetter", bestilling.bestillBillet(antallBilleter));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 6, 99})
+    void en_bestilling_skal_si_ifra_om_det_er_tomt_for_billetter_test(int antallBilleter){
+        bestilling.setAntallBilletter(0);
+        assertEquals("Det er desverre tomt for billetter", bestilling.bestillBillet(antallBilleter));
+
+    }
+
+
 
     @Test
     void pris_for_alle_billetter_valgt_er_korrekt_test(){
