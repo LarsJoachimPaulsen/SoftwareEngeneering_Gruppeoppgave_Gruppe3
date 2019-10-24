0daf783e399b9bf22815cc8ef8992a03619168bb
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-10-24 18:14:28 +0200

    test for passordhash

---
+++ test/no/hiof/gruppe3/Modell/BrukerTest.java
@@ -0,0 +1,16 @@
+package no.hiof.gruppe3.Modell;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrukerTest {
+
+    Bruker bruker = new Bruker();
+
+    @Test
+    void passord_skal_gis_en_spesifik_hash_test(){
+        assertEquals("112fab0fb33df288fa7961d4479c03d9", bruker.MD5Hashing("drossap"));
+    }
+
+}
