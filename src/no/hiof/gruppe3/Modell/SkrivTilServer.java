47d86346f587f6913c471bcb77f38f66e80e06ed
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-10-24 20:06:23 +0200

    fungerende, pluss sudokode

--- src/no/hiof/gruppe3/Modell/SkrivTilServer.java
+++ src/no/hiof/gruppe3/Modell/SkrivTilServer.java
@@ -1,15 +1,17 @@
 package no.hiof.gruppe3.Modell;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public abstract class SkrivTilServer {
 
-    public int lesFraServer(String filsti){
+
+    // finne en måte å lage en generell filleser, som kan lese variernde størrelser på filer.
+    public String lesFraServer(String filsti){
         String linje = " ";
-        String cvsSplittVed = ";";
+        String cvsSplittVed = "¤";
+
+        String fraFil = "";
 
         try{
             BufferedReader lesFraFil = new BufferedReader(new FileReader(filsti));
@@ -18,34 +20,54 @@ public abstract class SkrivTilServer {
 
                 String[] output = linje.split(cvsSplittVed);
 
-                return Integer.parseInt(output[0]);
+                fraFil += output[0];
+
+                // en metode som går gjennom fila og teller antall cvsSplittVed, og endrer hvor mye
+                // og hva som skrives, etter behov.
+
             }
 
         }
         catch(FileNotFoundException FNFE){
             FNFE.printStackTrace();
 
-            return 0;
+
         }
         catch (IOException IOE){
             IOE.printStackTrace();
 
-            return 0;
+
         }
-        return 0;
+        return fraFil;
     }
 
-    /*
-    public void skrivTilServer(String filsti){
+ /*
+    public void skrivTilServer(String filsti, ArrayList<Object> liste){
 
         try{
-            BufferedWriter skrivTilServer = new BufferedWriter(new FileWriter());
+            BufferedWriter skrivTilServer = new BufferedWriter(new FileWriter(filsti));
 
-            skrivTilServer.write();
+            StringBuilder byggStrenger = new StringBuilder();
+
+            for(int i = 0; i<= liste.size(); i++){
+
+                //Løkke som går gjennom ArrayList, og løkke som går gjennom objektet og skriver til fil.      //
+                for(int j = 0; j < liste.indexOf(i).size(); j++ ) {
+                    byggStrenger.append(j);
+                }
+                byggStrenger.append("¤");
+
+            }
+
+
+            skrivTilServer.write(byggStrenger.toString());
 
         }
         catch(FileNotFoundException FNFE){
             FNFE.printStackTrace();
+        }
+        catch (IOException IOE){
+            IOE.printStackTrace();
         }
 
     } */
