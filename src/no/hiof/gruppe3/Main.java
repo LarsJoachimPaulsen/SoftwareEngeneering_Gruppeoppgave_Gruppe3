a28d7555757cf1279a8e3fcee2ca6a6e0e7e97b8
AdamAR <arafa170@gmail.com>
2019-10-31 15:24:56 +0100

    Tried skrivTilFil method and it works fine. notice: you don't have to create arrangement.ser or bruker.ser, as it will be created auto.

--- src/no/hiof/gruppe3/Main.java
+++ src/no/hiof/gruppe3/Main.java
@@ -3,10 +3,15 @@ package no.hiof.gruppe3;
 import no.hiof.gruppe3.Modell.Arrangement;
 import no.hiof.gruppe3.Modell.Bruker;
 import no.hiof.gruppe3.Modell.Innstillinger;
+import no.hiof.gruppe3.Modell.SkrivTilFil;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
+
+    private static ArrayList<Bruker> brukerListe = new ArrayList<Bruker>();
+    private static ArrayList<Arrangement> arrangementListe = new ArrayList<Arrangement>();
 
     public static void main(String[] args){
 
@@ -34,13 +39,73 @@ public class Main {
 
         //*************demo************//
 
-        Arrangement arr = new Arrangement("Løping","Fredrikstad", LocalDate.of(2019,11,10),150,200);
+        Arrangement r = new Arrangement("Løping","Fredrikstad", LocalDate.of(2019,11,10),150,200);
 
-        //Arrangement arr1 = new Arrangement("Ski","Fredrikstad", LocalDate.of(2019,10,10),150,200);
+        //Arrangement r1 = new Arrangement("Ski","Fredrikstad", LocalDate.of(2019,10,10),150,200);
 
-        System.out.println(arr);
+        System.out.println(r);
         //System.out.println(arr1);
+
+        //***************** noen arrangementer og brukere for å teste skriving til filen. ***********//
+
+        //legger til noen arrangementer til arrangementListe som ligger helt øverst i Main klasse.
+        arrangementListe.add(new Arrangement("Playing","Halden",LocalDate.of(2019,11,27),200,170));
+        arrangementListe.add(new Arrangement("ski","Mysen",LocalDate.of(2020,11,10),150,100));
+
+        //lager noen bruker på den måten istenden for brukerListe.add(new Bruker) for å kunne bruke bruker navn til å legge til arrangementer.
+        Bruker jon = new Bruker("AAA","Jon","Antonsen","az@gmail.com","12345",28);
+        Bruker kent = new Bruker("MMM","Kent","Østby","ty@hotmail.com","9870",32);
+
+
+        //**                            @@ LES MEG @@
+
+        // jeg får null pointer exception når jeg ligger arrangement/er til bruker på denne måten(se nede)
+        // når jeg kjører    System.out.println(arrangementListe.get(0)); så får jeg riktig/forventet svar
+        //fint om noen kan komme på noe forslag.
+        //
+        //**
+
+
+        //bruker jon skal ha 2 arrangementer som han skal på.
+
+        //jon.getArrangementerBrukerSkalPaa().add(arrangementListe.get(0));
+        //jon.getArrangementerBrukerSkalPaa().add(arrangementListe.get(1));
+
+
+        //bruker kent skl ha 1 arrangement som ha skal på.
+
+        //kent.getArrangementerBrukerSkalPaa().add(arrangementListe.get(1));
+
+        //legger til noen bruker til brukerListe som ligger helt øverst i Main klasse.
+        brukerListe.add(jon);
+        brukerListe.add(kent);
+
+        //lager en tøm arrayListe av Objekter som blir fylt med objekter fra arrangementListe.
+        //viktig å merke at listene inneholder objekter og ikke arrangementer.
+        ArrayList<Object> arrangementObjListe = new ArrayList<Object>();
+        for(Arrangement arr: arrangementListe){
+            arrangementObjListe.add(arr);
+        }
+
+        //lager en tøm arrayListe av Objekter som blir fylt med objekter fra brukerListe.
+        //viktig å merke at listene inneholder objekter og ikke brukere.
+        ArrayList<Object> brukerObjListe = new ArrayList<>();
+        for(Bruker b: brukerListe){
+            brukerObjListe.add(b);
+        }
+
+
+        new SkrivTilFil("arrangement.ser",arrangementObjListe);
+        new SkrivTilFil("bruker.ser",brukerObjListe);
+
+
+
 
 
     }
+
+    public static ArrayList<Bruker> getBrukerListe() { return brukerListe; }
+
+    public static ArrayList<Arrangement> getArrangementListe() {return arrangementListe;}
+
 }
