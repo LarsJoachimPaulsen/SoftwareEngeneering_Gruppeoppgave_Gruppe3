d2405598a0ea37a28afbc7ec43f4490071898caa
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-11-01 18:30:28 +0100

    Lagt inn noen metoder

--- src/no/hiof/gruppe3/Modell/Klubb.java
+++ src/no/hiof/gruppe3/Modell/Klubb.java
@@ -5,5 +5,27 @@ import java.util.ArrayList;
 public class Klubb {
 
     private String navn;
+
+    //Muligheter for flere kontakpersoner, en liste her istedenfor?
+    private Bruker kontaktperson;
     private ArrayList<Bruker> brukerliste = new ArrayList<Bruker>();
+    private ArrayList<String> sportsliste = new ArrayList<String>();
+
+
+
+
+
+    public void leggTilSport(String nySport){
+
+        sportsliste.add(nySport);
+
+    }
+
+    public void leggTilBruker(Bruker nyBruker){
+
+        brukerliste.add(nyBruker);
+
+    }
+
+
 }
