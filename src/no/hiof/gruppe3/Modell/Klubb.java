be8bfbc8b24994e6ed36579698e6e13557b035c0
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-11-01 18:24:54 +0100

    Lagt inn telefonnummer som en variabel

--- src/no/hiof/gruppe3/Modell/Klubb.java
+++ src/no/hiof/gruppe3/Modell/Klubb.java
@@ -2,7 +2,7 @@ package no.hiof.gruppe3.Modell;
 
 import java.util.ArrayList;
 
-public class Klubb  {
+public class Klubb {
 
     private String navn;
     private ArrayList<Bruker> brukerliste = new ArrayList<Bruker>();
