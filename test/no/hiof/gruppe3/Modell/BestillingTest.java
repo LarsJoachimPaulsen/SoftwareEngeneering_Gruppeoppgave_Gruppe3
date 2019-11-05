da6e21a38db9c369809668b3b2a76107404f5e20
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2019-11-05 10:06:30 +0100

    added params file

--- test/no/hiof/gruppe3/Modell/BestillingTest.java
+++ test/no/hiof/gruppe3/Modell/BestillingTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class BestillingTest {
 
